167c7aacc5fa1aa066decc6f9baf080160b67f5e
kawsar <96182258+kawsarbp@users.noreply.github.com>
2022-12-03 15:31:53 +0600

    go to join oparation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,24 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'id',
-        'category_id',
-        'title',
-        'slug',
-        'description',
-        'photo',
-        'satus',
-    ];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
 }
